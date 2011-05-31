99227bb346c4cee66a5d262eaa757ddfed3add65
Tobias Falkberger <falkberger@gmail.com>
2011-05-31 13:42:56 +0200

    test

--- Calculator/CalculatorViewController.h
+++ Calculator/CalculatorViewController.h
@@ -1,8 +1,8 @@
 //
-//  CalculatorViewController.h
-//  Calculator
+//  TestViewController.h
+//  Test
 //
-//  Created by Tobias Falkberger on 5/31/11.
+//  Created by Tobias on 5/25/11.
 //  Copyright 2011 __MyCompanyName__. All rights reserved.
 //
 
@@ -10,6 +10,26 @@
 
 @interface CalculatorViewController : UIViewController {
     
+    IBOutlet UIButton *button_1;
+    IBOutlet UIButton *button_2;
+    IBOutlet UIButton *button_3;
+    IBOutlet UIButton *button_4;
+    IBOutlet UIButton *button_5;
+    IBOutlet UIButton *button_6;
+    IBOutlet UIButton *button_7;
+    IBOutlet UIButton *button_8;
+    IBOutlet UIButton *button_9;
+    IBOutlet UIButton *button_0;
+    IBOutlet UIButton *button_add;
+    IBOutlet UIButton *button_sub;
+    IBOutlet UIButton *button_mul;
+    IBOutlet UIButton *button_div;
+    IBOutlet UIButton *button_eq;
+    IBOutlet UIButton *button_del;
+    IBOutlet UITextField *text;
+    
 }
-
+-(IBAction)buttonPressed:(id)sender; 
+-(IBAction)calc;
+-(IBAction)del;
 @end
--- Calculator/CalculatorViewController.m
+++ Calculator/CalculatorViewController.m
@@ -1,8 +1,8 @@
 //
-//  CalculatorViewController.m
-//  Calculator
+//  TestViewController.m
+//  Test
 //
-//  Created by Tobias Falkberger on 5/31/11.
+//  Created by Tobias on 5/25/11.
 //  Copyright 2011 __MyCompanyName__. All rights reserved.
 //
 
@@ -43,7 +43,85 @@
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
 {
     // Return YES for supported orientations
-    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+    return YES;
 }
 
+-(IBAction)buttonPressed:(id)sender {
+    text.text = [NSString stringWithFormat:@"%@%@", text.text, [(UIButton *)sender currentTitle]];
+}
+
+-(IBAction)calc {
+    NSString *exp = [text text];
+    int res;
+   
+    
+    NSArray *values = [exp componentsSeparatedByCharactersInSet:
+                        [NSCharacterSet characterSetWithCharactersInString:@"+-*/"]
+                        ];
+    NSMutableArray *ops = [[NSMutableArray alloc] init]; 
+    
+    NSNumberFormatter * f = [[NSNumberFormatter alloc] init];
+
+    
+    for (int i=0; i < [exp length]; i++) {
+
+        NSString *ichar  = [NSString stringWithFormat:@"%c", [exp characterAtIndex:i]];
+
+        
+        if ([f numberFromString: ichar] == nil) {
+           		[ops addObject:ichar];
+        }
+        
+    }
+     NSLog(@"%@",ops);
+     NSLog(@"%@",values);
+    
+   
+    
+    int x = 0;
+     for (int i = 0; i < [values count]; i++) {
+         
+          NSString *op = [ops objectAtIndex:x];
+         
+         if ([op isEqualToString:@"+"]) {
+             if (i == 0) {
+                 res =  [[values objectAtIndex:i] intValue] + [[values objectAtIndex:++i] intValue];
+             } else {
+                  res =  res + [[values objectAtIndex:i] intValue];
+             }
+            
+         } else if ([op isEqualToString:@"-"]) {
+             if (i == 0) {
+                 res =  [[values objectAtIndex:i] intValue] - [[values objectAtIndex:++i] intValue];
+             } else {
+                 res =  res - [[values objectAtIndex:i] intValue];
+             }
+             
+         } else if ([op isEqualToString:@"*"]) {
+             if (i == 0) {
+                 res =  [[values objectAtIndex:i] intValue] * [[values objectAtIndex:++i] intValue];
+             } else {
+                 res =  res * [[values objectAtIndex:i] intValue];
+             }
+             
+         } else if ([op isEqualToString:@"/"]) {
+             if (i == 0) {
+                 res =  [[values objectAtIndex:i] intValue] / [[values objectAtIndex:++i] intValue];
+             } else {
+                 res =  res / [[values objectAtIndex:i] intValue];
+             }
+         }
+     }
+    text.text = [NSString stringWithFormat:@"%d", res];
+    [f release];
+    [ops release];
+    
+}
+
+-(IBAction)del {
+    if ([text.text length] > 0) {
+        text.text = [text.text substringToIndex:[text.text length] - 1];
+    }
+    
+}
 @end
